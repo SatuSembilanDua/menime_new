437a37cfa2923731d7830db2ce2a854ae6872d49
SatuSembilanDua <satusembilandua.168@gmail.com>
2026-05-17 12:13:46 +0700

    fixing suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 import Riwayat from "@/hooks/riwayat";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,7 +56,9 @@ export default function RootLayout({
     >
       <body>
         {children}
-        <Riwayat />
+        <Suspense>
+          <Riwayat />
+        </Suspense>
       </body>
     </html>
   );
